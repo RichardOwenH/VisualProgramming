ee398580b1cc338ff34ce84e5dc95d719ac4b68e
Richard Owen H <75958078+RichardOwenH@users.noreply.github.com>
2021-12-13 21:08:05 +0700

    Payroll_Accounting_System
    A small project that uses the Visual Basic programming language to demonstrate an arithmethic operation.

---
+++ Payroll Accounting System/Payroll Accounting System/Form1.Designer.vb
@@ -0,0 +1,319 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.txthours1 = New System.Windows.Forms.TextBox()
+        Me.txthours2 = New System.Windows.Forms.TextBox()
+        Me.txthours4 = New System.Windows.Forms.TextBox()
+        Me.txthours3 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblWeeklyPay1 = New System.Windows.Forms.Label()
+        Me.lblWeeklyPay2 = New System.Windows.Forms.Label()
+        Me.lblWeeklyPay3 = New System.Windows.Forms.Label()
+        Me.lblWeeklyPay4 = New System.Windows.Forms.Label()
+        Me.lblTotalPay = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.BackColor = System.Drawing.Color.LightCoral
+        Me.btnCalculate.Font = New System.Drawing.Font("Mojito in June", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.ForeColor = System.Drawing.Color.Snow
+        Me.btnCalculate.Location = New System.Drawing.Point(94, 385)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(117, 36)
+        Me.btnCalculate.TabIndex = 0
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Mojito in June", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.IndianRed
+        Me.TextBox1.Location = New System.Drawing.Point(94, 153)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(172, 31)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Japanese 2020", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.IndianRed
+        Me.Label1.Location = New System.Drawing.Point(119, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(470, 25)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Payroll Accounting System"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Mojito in June", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.IndianRed
+        Me.Label2.Location = New System.Drawing.Point(91, 107)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(162, 26)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Employee Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Mojito in June", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.IndianRed
+        Me.Label3.Location = New System.Drawing.Point(303, 107)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(135, 26)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Hours Worked"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Mojito in June", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.IndianRed
+        Me.Label4.Location = New System.Drawing.Point(493, 107)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(116, 26)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Weekly Pay"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Mojito in June", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.ForeColor = System.Drawing.Color.IndianRed
+        Me.TextBox2.Location = New System.Drawing.Point(94, 202)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(172, 31)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Mojito in June", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.ForeColor = System.Drawing.Color.IndianRed
+        Me.TextBox3.Location = New System.Drawing.Point(94, 259)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(172, 31)
+        Me.TextBox3.TabIndex = 7
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("Mojito in June", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox4.ForeColor = System.Drawing.Color.IndianRed
+        Me.TextBox4.Location = New System.Drawing.Point(94, 314)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(172, 31)
+        Me.TextBox4.TabIndex = 8
+        '
+        'txthours1
+        '
+        Me.txthours1.Font = New System.Drawing.Font("Montserrat", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txthours1.ForeColor = System.Drawing.Color.IndianRed
+        Me.txthours1.Location = New System.Drawing.Point(306, 153)
+        Me.txthours1.Name = "txthours1"
+        Me.txthours1.Size = New System.Drawing.Size(116, 28)
+        Me.txthours1.TabIndex = 9
+        '
+        'txthours2
+        '
+        Me.txthours2.Font = New System.Drawing.Font("Montserrat", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txthours2.ForeColor = System.Drawing.Color.IndianRed
+        Me.txthours2.Location = New System.Drawing.Point(306, 202)
+        Me.txthours2.Name = "txthours2"
+        Me.txthours2.Size = New System.Drawing.Size(116, 28)
+        Me.txthours2.TabIndex = 10
+        '
+        'txthours4
+        '
+        Me.txthours4.Font = New System.Drawing.Font("Montserrat", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txthours4.ForeColor = System.Drawing.Color.IndianRed
+        Me.txthours4.Location = New System.Drawing.Point(306, 314)
+        Me.txthours4.Name = "txthours4"
+        Me.txthours4.Size = New System.Drawing.Size(116, 28)
+        Me.txthours4.TabIndex = 12
+        '
+        'txthours3
+        '
+        Me.txthours3.Font = New System.Drawing.Font("Montserrat", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txthours3.ForeColor = System.Drawing.Color.IndianRed
+        Me.txthours3.Location = New System.Drawing.Point(306, 259)
+        Me.txthours3.Name = "txthours3"
+        Me.txthours3.Size = New System.Drawing.Size(116, 28)
+        Me.txthours3.TabIndex = 17
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Mojito in June", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.IndianRed
+        Me.Label5.Location = New System.Drawing.Point(375, 391)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(100, 25)
+        Me.Label5.TabIndex = 18
+        Me.Label5.Text = "Total Pay"
+        '
+        'lblWeeklyPay1
+        '
+        Me.lblWeeklyPay1.AutoSize = True
+        Me.lblWeeklyPay1.Font = New System.Drawing.Font("Montserrat", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWeeklyPay1.ForeColor = System.Drawing.Color.IndianRed
+        Me.lblWeeklyPay1.Location = New System.Drawing.Point(501, 153)
+        Me.lblWeeklyPay1.Name = "lblWeeklyPay1"
+        Me.lblWeeklyPay1.Size = New System.Drawing.Size(21, 20)
+        Me.lblWeeklyPay1.TabIndex = 19
+        Me.lblWeeklyPay1.Text = "0"
+        '
+        'lblWeeklyPay2
+        '
+        Me.lblWeeklyPay2.AutoSize = True
+        Me.lblWeeklyPay2.Font = New System.Drawing.Font("Montserrat", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWeeklyPay2.ForeColor = System.Drawing.Color.IndianRed
+        Me.lblWeeklyPay2.Location = New System.Drawing.Point(501, 205)
+        Me.lblWeeklyPay2.Name = "lblWeeklyPay2"
+        Me.lblWeeklyPay2.Size = New System.Drawing.Size(21, 20)
+        Me.lblWeeklyPay2.TabIndex = 20
+        Me.lblWeeklyPay2.Text = "0"
+        '
+        'lblWeeklyPay3
+        '
+        Me.lblWeeklyPay3.AutoSize = True
+        Me.lblWeeklyPay3.Font = New System.Drawing.Font("Montserrat", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWeeklyPay3.ForeColor = System.Drawing.Color.IndianRed
+        Me.lblWeeklyPay3.Location = New System.Drawing.Point(501, 259)
+        Me.lblWeeklyPay3.Name = "lblWeeklyPay3"
+        Me.lblWeeklyPay3.Size = New System.Drawing.Size(21, 20)
+        Me.lblWeeklyPay3.TabIndex = 21
+        Me.lblWeeklyPay3.Text = "0"
+        '
+        'lblWeeklyPay4
+        '
+        Me.lblWeeklyPay4.AutoSize = True
+        Me.lblWeeklyPay4.Font = New System.Drawing.Font("Montserrat", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWeeklyPay4.ForeColor = System.Drawing.Color.IndianRed
+        Me.lblWeeklyPay4.Location = New System.Drawing.Point(501, 317)
+        Me.lblWeeklyPay4.Name = "lblWeeklyPay4"
+        Me.lblWeeklyPay4.Size = New System.Drawing.Size(21, 20)
+        Me.lblWeeklyPay4.TabIndex = 22
+        Me.lblWeeklyPay4.Text = "0"
+        '
+        'lblTotalPay
+        '
+        Me.lblTotalPay.AutoSize = True
+        Me.lblTotalPay.Font = New System.Drawing.Font("Montserrat", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalPay.ForeColor = System.Drawing.Color.IndianRed
+        Me.lblTotalPay.Location = New System.Drawing.Point(501, 391)
+        Me.lblTotalPay.Name = "lblTotalPay"
+        Me.lblTotalPay.Size = New System.Drawing.Size(21, 20)
+        Me.lblTotalPay.TabIndex = 23
+        Me.lblTotalPay.Text = "0"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.LightCoral
+        Me.Button1.Font = New System.Drawing.Font("Mojito in June", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Snow
+        Me.Button1.Location = New System.Drawing.Point(94, 77)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(525, 18)
+        Me.Button1.TabIndex = 24
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.LightCoral
+        Me.Button2.Font = New System.Drawing.Font("Mojito in June", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Snow
+        Me.Button2.Location = New System.Drawing.Point(94, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(525, 19)
+        Me.Button2.TabIndex = 25
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Snow
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblTotalPay)
+        Me.Controls.Add(Me.lblWeeklyPay4)
+        Me.Controls.Add(Me.lblWeeklyPay3)
+        Me.Controls.Add(Me.lblWeeklyPay2)
+        Me.Controls.Add(Me.lblWeeklyPay1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txthours3)
+        Me.Controls.Add(Me.txthours4)
+        Me.Controls.Add(Me.txthours2)
+        Me.Controls.Add(Me.txthours1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Name = "Form1"
+        Me.Text = "Payroll Accounting System"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents txthours1 As TextBox
+    Friend WithEvents txthours2 As TextBox
+    Friend WithEvents txthours4 As TextBox
+    Friend WithEvents txthours3 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblWeeklyPay1 As Label
+    Friend WithEvents lblWeeklyPay2 As Label
+    Friend WithEvents lblWeeklyPay3 As Label
+    Friend WithEvents lblWeeklyPay4 As Label
+    Friend WithEvents lblTotalPay As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Payroll Accounting System/Payroll Accounting System/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Inherits System.Windows.Forms.Form
+    Const sngPayRate As Single = 9.5
+
+    Private Sub bbtnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim decTotalPay As Decimal
+        Dim decWeeklyPay As Decimal
+
+        decWeeklyPay = Val(txthours1.Text) * sngPayRate
+        lblWeeklyPay1.Text = decWeeklyPay.ToString
+        decTotalPay = decWeeklyPay
+
+        decWeeklyPay = Val(txthours2.Text) * sngPayRate
+        lblWeeklyPay2.Text = decWeeklyPay.ToString
+        decTotalPay += decWeeklyPay
+
+        decWeeklyPay = Val(txthours3.Text) * sngPayRate
+        lblWeeklyPay3.Text = decWeeklyPay.ToString
+        decTotalPay += decWeeklyPay
+
+        decWeeklyPay = Val(txthours4.Text) * sngPayRate
+        lblWeeklyPay4.Text = decWeeklyPay.ToString
+        decTotalPay += decWeeklyPay
+
+        lblTotalPay.Text = decTotalPay.ToString
+
+    End Sub
+
+End Class
